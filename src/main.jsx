cd5d2412c74f1fe742f052d548aab524c3debd4f
Ahrar Shah <shahbakers786@gmail.com>
2025-10-09 16:11:07 +0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import SmartCityScene from './App.jsx'   // this is the SmartCity.jsx code you pasted
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SmartCityScene />
+  </React.StrictMode>
+)
